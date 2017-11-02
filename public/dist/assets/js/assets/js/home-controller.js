3dad86b802da9ea05aaec152012451a8b0d328f5
J2A2B <ja.andreani@gmail.com>
2017-11-03 00:48:23 +0100

    first push vps

--- public/dist/assets/js/assets/js/home-controller.js
+++ public/dist/assets/js/assets/js/home-controller.js
@@ -8,33 +8,25 @@ app.controller('home-controller', ['$scope','$http', '$routeParams', 'dataEp', f
   $scope.showInfosEp = (ep) =>{
     $scope.showImg = false;
     $scope.eps = ep.photos;
-  console.log($scope.eps)
-}
-// $scope.leftSlide = () =>{
-//  $(".slideshow ul").animate({marginLeft:-400},800,function(){
-//   $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-// })
-// }
-// $scope.rightSlide = () =>{
-//  $(".slideshow ul").animate({marginRight:-400},800,function(){
-//   $(this).css({marginRight:0}).find("li:last").after($(this).find("li:first"));
-// })
-// }
-$scope.leftSlide = () =>{
-  
-  const arrayImg = $scope.eps;
-  // console.log(arrayImg)
-  const lastElement = arrayImg.slice(arrayImg.length-1);
-  // console.log(lastElement)
-  const arrayRemoved = arrayImg.splice(0,arrayImg.length -1)
-  $scope.eps = lastElement.concat(arrayRemoved); 
-  console.log($scope.eps)
-}
-$scope.rightSlide = () =>{
-  const arrayImg = $scope.eps;
-  const firstElement = arrayImg.slice(0,1);
-  const arrayRemoved = arrayImg.splice(1,arrayImg.length -1)
-   $scope.eps = arrayRemoved.concat(firstElement); 
-}
-$( ".slideshow " ).draggable();
-}]);﻿
+  }
+  $scope.leftSlide = () =>{
+    const arrayImg = $scope.eps;
+    const lastElement = arrayImg.slice(arrayImg.length-1);
+    const arrayRemoved = arrayImg.slice(0,arrayImg.length-1);
+    $scope.eps = lastElement.concat(arrayRemoved); 
+  }
+  $scope.rightSlide = () =>{
+    const arrayImg = $scope.eps;
+    const firstElement = arrayImg.slice(0,1);
+    const arrayRemoved = arrayImg.slice(1,arrayImg.length+1)
+    $scope.eps = arrayRemoved.concat(firstElement); 
+  }
+  $scope.dragSlideshow = () =>{
+    $( ".slideshow" ).draggable();
+  }
+
+    //   setTimeout(function(){
+    //     $( ".slideshow" ).draggable();
+    //     // console.log('After:', $('.sortable').length);
+    // },1000)
+  }]);﻿
--- public/src/assets/js/about-controller.js
+++ public/src/assets/js/about-controller.js
@@ -1,9 +1,30 @@
-app.controller('about-controller', ['$scope','$http', 'dataAbout', function($scope,$http,dataAbout) {
-dataAbout.getInfoBio().then((res) => {
+app.controller('about-controller', ['$scope','$http','$sce', '$timeout','dataAbout', function($scope,$http,$sce,$timeout,dataAbout) {
+  const BASE_URL_SOUNDCLOUD = "https://w.soundcloud.com/player/?url=//soundcloud.com/";
+  const END_URL_SOUNDCLOUD = "&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true";
+  $scope.showWindow=true;
+  dataAbout.getInfoBio().then((res) => {
     $scope.infoBio=res.data;
-})
-dataAbout.getInfoEvents().then((res) => {
+  })
+  dataAbout.getInfoEvents().then((res) => {
     $scope.infoEvents=res.data;
-    console.log($scope.infoEvents)
-})
+  })
+  $scope.getIdEvent =(event, input) =>{
+$timeout(function () {
+  $scope.showWindow=false ;
+}, 500);
+console.log(event.podcasts[0].url.length)
+if (input === "one") {
+  $scope.fullUrlSoundcloud = $sce.trustAsResourceUrl(BASE_URL_SOUNDCLOUD+event.podcasts[0].url+END_URL_SOUNDCLOUD);
+  $scope.podcastName = event.podcasts[0].name;
+}else {
+  $scope.fullUrlSoundcloud = $sce.trustAsResourceUrl(BASE_URL_SOUNDCLOUD+event.podcasts[1].url+END_URL_SOUNDCLOUD);
+  $scope.podcastName = event.podcasts[1].name;
+}
+
+// $sce.trustAsResourceUrl(BASE_URL_SOUNDCLOUD+event.podcasts[0].url+END_URL_SOUNDCLOUD);
+// $scope.nameSoundcloud =
+  }
+  $scope.dragSlideshow = () =>{
+    $( ".cont-window-about" ).draggable();
+  }
 }]);﻿
